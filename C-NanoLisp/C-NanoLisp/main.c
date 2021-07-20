751c31a0f7e64cdd1ba4835ac95a1b219ee296f4
Jean-Pierre de SOZA <ds.jaypee@gmail.com>
2021-07-20 10:11:52 +0200

    Déplacé vers un repo propre nanolispc

---
+++ C-NanoLisp/C-NanoLisp/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C-NanoLisp
+//
+//  Created by Jean-Pierre De Soza on 19/07/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
